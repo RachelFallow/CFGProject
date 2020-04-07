31d92943a2a249ea0847a666a412e6452fb8b245
alicerwoodman <61164674+alicerwoodman@users.noreply.github.com>
2020-04-07 14:56:44 +0100

    Update java.js
    Tested it yesterday - deleted first prompt otherwise 2 prompts come up

--- java.js
+++ java.js
@@ -1,4 +1,3 @@
-prompt("Want to get weekly updates about women's sport?")
 var answer = prompt("Want to get weekly updates about women's sport? Answer 'yes' or 'no'.")
 if(answer = "yes") {
     prompt("Leave your email address in the box below! :)") 
